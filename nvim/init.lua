a4146dd4f3d3498cf985a36acfd15e88cfd7a851
UKouji555 <zerozero53205320.dev@gmail.com>
2025-03-10 04:06:59 +0900

    補完の設定追加

--- nvim/init.lua
+++ nvim/init.lua
@@ -37,7 +37,6 @@ if dpp.load_state(dpp_base) then
   })
 end
 
--- これはなくても大丈夫です。
 vim.api.nvim_create_autocmd("User", {
 	pattern = "Dpp:makeStatePost",
 	callback = function ()
@@ -47,3 +46,12 @@ vim.api.nvim_create_autocmd("User", {
 
 require("keymaps")
 require("settings")
+
+vim.api.nvim_create_augroup("MyLSPAutoFormat", { clear = true })
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = { "*.lua" },
+  callback = function()
+    vim.lsp.buf.format({ async = false })
+  end,
+  group = "MyLSPAutoFormat",
+})
